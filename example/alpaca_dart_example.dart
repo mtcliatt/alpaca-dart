2f5ed7484256d4b7f014204acaaf12d1fc847822
mtcliatt <matthewcliatt@gmail.com>
2019-03-24 20:04:41 -0700

    Update example code.

--- example/alpaca_dart_example.dart
+++ example/alpaca_dart_example.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 
-import 'package:alpaca_dart/src/alpaca_api.dart';
+import 'package:alpaca_dart/alpaca_dart.dart';
 
 main() async {
-  var api = AlpacaApi(
+  var alpaca = AlpacaApi(
     keyId: '...',
     secretKey: '...',
+    paperTrading: true,
   );
 
-  final accountResponse = await api.getAccount();
+  final accountResponse = await alpaca.getAccount();
   final account = jsonDecode(accountResponse.body);
 
   final status = account['status'] == 'ACTIVE' ? 'Active' : 'Inactive';
@@ -17,5 +18,19 @@ main() async {
   print('Account status: $status');
   print('Cash value: ${account['cash']}');
   print('Portfolio value: ${account['portfolio_value']}');
-}
+  print('\n');
 
+  final watchlist = ['SPY', 'DIA', 'QQQ'];
+  final timeframe = 'day', limit = 2;
+
+  final barsResponse = await alpaca.getBars(timeframe, watchlist, limit: limit);
+  final barsJson = jsonDecode(barsResponse.body);
+
+  for (final symbol in watchlist) {
+    final bars = barsJson[symbol];
+
+    for (final bar in bars) {
+      print('(${bar['t']}): ${bar['c']}');
+    }
+  }
+}
